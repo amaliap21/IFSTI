99b19fe3296b2e58a139b541f65faf5021e5d6ce
Amalia Putri <amaliaputriii2104@gmail.com>
2024-04-01 03:28:37 +0700

    feat: build modules

---
+++ website/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main
+      className="flex min-h-screen flex-col justify-between 
+    "
+      // px-24 pt-8 pb-24
+    ></main>
+  );
+}
