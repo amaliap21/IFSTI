18cdccd7bbcf6997a70ebf6cba7498dcc4b5a588
Vktr12104 <116500975+Vktr12104@users.noreply.github.com>
2024-05-10 19:31:07 +0700

    Commit Code IoT

---
+++ IoT/Project.cpp
@@ -0,0 +1,118 @@
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "AndroidAP2E05"
+#define WIFI_PASSWORD "yeeg1062"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyBb8zvL44s1MN_4XMQ4THl5ezQE1GPp0_Y"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://embedded-3d80d-default-rtdb.asia-southeast1.firebasedatabase.app/ " 
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+int count = 0;
+bool signupOK = false;
+
+int _moisture,sensor_analog;
+const int sensor_pin = 13;  /* Soil moisture sensor O/P pin */
+const int potPin=14;
+float ph;
+float Value=0;
+
+void setup(void){
+  Serial.begin(115200);  
+  pinMode(potPin,INPUT);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  delay(1000);   /* Set the baudrate to 115200*/
+}
+
+void loop(void){
+  sensor_analog = analogRead(sensor_pin);
+  _moisture = ( 100 - ( (sensor_analog/4095.00) * 100 ) );
+  Serial.print("Moisture = ");
+  Serial.print(_moisture);  /* Print Temperature on the serial window */
+  Serial.println("%");           /* Wait for 1000mS */
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+      sendDataPrevMillis = millis();
+      // Write an Int number on the database path test/int
+      if (Firebase.RTDB.setInt(&fbdo, "/label_soil", _moisture)){
+        Serial.println("PASSED");
+        Serial.println("PATH: " + fbdo.dataPath());
+        Serial.println("TYPE: " + fbdo.dataType());
+      }
+      else {
+        Serial.println("FAILED");
+        Serial.println("REASON: " + fbdo.errorReason());
+      }
+    }
+  Value= analogRead(potPin);
+  Serial.print(Value);
+  Serial.print(" | ");
+  float voltage=Value*(3.3/4095.0);
+  ph=(3.3*voltage);
+  Serial.println(ph);
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    // Write an Int number on the database path test/int
+    if (Firebase.RTDB.setInt(&fbdo, "/label_ph", ph)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
+  delay(1000);
+}
+
